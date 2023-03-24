21c7348c0ef5263c726ad492e3b8780b81d95243
eliasmurcray <eliasmurcray@gmail.com>
2023-03-24 09:05:59 -0700

    SVGWaves class added

--- src/tsx/index.tsx
+++ src/tsx/index.tsx
@@ -3,6 +3,26 @@ import { createRoot } from 'react-dom/client';
 import '../scss/index.scss';
 import { AppHeader, AppFooter, MobileNav } from './globals';
 
+type SVGWavesProps = {
+  fillColor?: string;
+};
+
+class SVGWaves extends Component<SVGWavesProps> {
+  render() {
+    return <svg className="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" fill={this.props.fillColor}>
+      <defs>
+        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+      </defs>
+      <g className="parallax">
+        <use xlinkHref="#gentle-wave" x="48" y="1" />
+        <use xlinkHref="#gentle-wave" x="48" y="3" />
+        <use xlinkHref="#gentle-wave" x="48" y="5" />
+        <use xlinkHref="#gentle-wave" x="48" y="7" />
+      </g>
+    </svg>;
+  }
+}
+
 class App extends Component {
   render() {
     return <div>
@@ -15,23 +35,18 @@ class App extends Component {
             <div>Empowering educators, enriching communities</div>
           </header>
       </section>
-      <section className="about">
-        <svg className="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" fill="#2e94d3">
-          <defs>
-            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-          </defs>
-          <g className="parallax">
-            <use xlinkHref="#gentle-wave" x="48" y="1" />
-            <use xlinkHref="#gentle-wave" x="48" y="3" />
-            <use xlinkHref="#gentle-wave" x="48" y="5" />
-            <use xlinkHref="#gentle-wave" x="48" y="7" />
-          </g>
-        </svg>
+      <section className="about" style={{ paddingBottom: "180px"}}>
+        <SVGWaves fillColor={"#2e94d3"} />
         <div>
           <h2>Mission Statement</h2>
           SBCTA is a union of professionals that value high-quality public education for our students, families, and communities. We are dedicated to protecting and enhancing equitable access, justice, and resources for all of San Bernardino County Superintendent of School's students, teachers, and classrooms. Working in collaboration with SBCSS, school districts, families, other agencies, and our community we provide leadership, advocacy, and services to ensure innovative, inspiring educational practice.<br />
           <a href="/members">View Board Members</a>
         </div>
+      </section>
+      <section className="news">
+        <SVGWaves fillColor={"#ffffff"} />
+        <h2>News</h2>
+        <div>No news... yet.</div>
       </section>
       <AppFooter />
     </div>;
